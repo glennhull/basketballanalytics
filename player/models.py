8be9fc23efcbcc059c219db716ef58337771b921
Farhan Attamimi <farhan.attamimi@gmail.com>
2017-04-13 12:15:04 -0700

    Get basic stats on player pages

--- player/models.py
+++ player/models.py
@@ -4,3 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Player(models.Model): 
+	player_name = models.CharField(max_length=200)
+
+class Season_Stats(models.Model):
+	player = models.ForeignKey(Player, on_delete=models.CASCADE)
+	ppg = models.DecimalField(max_digits=5, decimal_places=1)
+	apg = models.DecimalField(max_digits=5, decimal_places=1)
+	rpg = models.DecimalField(max_digits=5, decimal_places=1)
+	per = models.DecimalField(max_digits=5, decimal_places=2)
--- player/views.py
+++ player/views.py
@@ -6,11 +6,23 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
+from stattlepy import Stattleship
+import json
 
 def player_page(request, player_name): 
+	New_query = Stattleship()
+	Token = New_query.set_token('46a96fa056bca68ddb8835f70ad68974')
+	player_id = "nba-%s" % player_name
+	output = New_query.ss_get_results(sport='basketball',league='nba', season_id='nba-2016-2017', ep='player_season_stats',player_id=player_id)
+	ppg = output[0]['player_season_stats'][0]['average_points']
+	apg = output[0]['player_season_stats'][0]['average_assists']
+	rpg = output[0]['player_season_stats'][0]['average_rebounds']
 	template = loader.get_template('player/index.html')
 	context = {
 		'player_name' : player_name,
+		'output': output[0]['player_season_stats'],
+		'ppg' : ppg,
+		'apg' : apg,
+		'rpg' : rpg,
 	}
 	return HttpResponse(template.render(context, request))
-
