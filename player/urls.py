fab3691e80aa816eba553519bd94936d6bd06db1
Farhan Attamimi <farhan.attamimi@gmail.com>
2017-04-23 15:20:27 -0700

    Link to player pages from team rosters

--- player/urls.py
+++ player/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url 
 from . import views 
 
+app_name = 'player'
+
 urlpatterns = [
-	url(r'^(?P<player_name>.*)', views.player_page, name='player'),
+	url(r'^(?P<player_name>.*)', views.player_page, name='player_page'),
 ]
