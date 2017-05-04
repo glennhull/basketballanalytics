a0c7ee274e962995998132ff93aaf4207f6160c9
Farhan Attamimi <farhan.attamimi@gmail.com>
2017-05-04 06:22:23 -0700

    Team page

--- team/views.py
+++ team/views.py
@@ -32,6 +32,12 @@ def team_page(request, team_slug):
 	fga = get_fga(team_id)
 	fgm = get_fgm(team_id)
 	tpg = get_tpg(team_id)
+	tpa = get_threeptattempted(team_id)
+	tpm = get_threeptmade(team_id)
+	fb = get_fastbreak(team_id)
+	pp = paint_points(team_id)
+	tp = turnover_points(team_id)
+	sc = secondchance_points(team_id)
 	context = {
 		'team_name' : name,
 		'team_slug' : team_slug,
@@ -44,7 +50,13 @@ def team_page(request, team_slug):
 		'fga' : fga,
 		'fgm' : fgm,
 		'tpg' : tpg,
-	}
+		'tpa' : tpa,
+		'tpm' : tpm,
+		'fb' : fb,
+		'pp' : pp,
+		'tp' : tp,
+		'sc' : sc, 
+ 	}
 	return HttpResponse(template.render(context, request))
 
 # returns dictionary with player name as key and position as value
@@ -87,3 +99,27 @@ def get_fgm(team_id):
 def get_tpg(team_id):
 	tpg = Team.objects.filter(team_id=team_id).values('tpg')[0]['tpg']
 	return tpg
+
+def get_threeptattempted(team_id):
+	tpa = Team.objects.filter(team_id=team_id).values('threept_attempted')[0]['threept_attempted']
+	return tpa
+
+def get_threeptmade(team_id):
+	tpm = Team.objects.filter(team_id=team_id).values('threept_made')[0]['threept_made']
+	return tpm
+
+def get_fastbreak(team_id):
+	tpm = Team.objects.filter(team_id=team_id).values('fastbreak_points')[0]['fastbreak_points']
+	return tpm
+
+def paint_points(team_id):
+	pp = Team.objects.filter(team_id=team_id).values('paint_points')[0]['paint_points']
+	return pp
+
+def turnover_points(team_id):
+	tp = Team.objects.filter(team_id=team_id).values('turnover_points')[0]['turnover_points']
+ 	return tp
+
+def secondchance_points(team_id):
+	ss = Team.objects.filter(team_id=team_id).values('secondchance_points')[0]['secondchance_points']
+ 	return ss
